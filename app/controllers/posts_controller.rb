1190778c6bac180dac7ae9c95313e931235a40fd
github username <jhernandez1692@gmail.com>
2020-10-13 15:21:34 -0400

    Implemented Create/ Read functionality for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,44 @@ class PostsController < ApplicationController
 
     # CRUD
 
-    # READ
-    # index route for all posts
-    # show route for single posts
 
-    get '/posts' do
-        # render all posts
-        @posts = Post.all
-        
-        erb :'/posts/index'
-    end
 
-    get '/posts/:id' do
-        @post = Post.find(params[:id])
-        erb :'/posts/show'
-    end
 
 
     # CREATE
+
+    get '/posts/new' do
+
+        erb :'/posts/new'
+    end
+
+    post '/posts' do
+        post = Post.create(title: params[:title], description: params[:description],
+        image_url: params[:image_url], user_id: current_user.id)    
+        
+        redirect "/posts/#{post.id}"
+    end
+
+    # ORDER MATTERS = READ Functionality needs CREATE to go first
+    # READ
+    # index route for all posts
+    
+
+    get '/posts' do
+        # render all posts
+        
+        @posts = Post.all
+        
+        
+        erb :'/posts/index'
+    end
+    # show route for single posts
+    get '/posts/:id' do
+        @post = Post.find(params[:id])
+        
+        erb :'/posts/show'
+    end
+
 
     # UPDATE
 
