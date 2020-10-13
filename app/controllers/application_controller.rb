eb9747b68a09fea94f23a5162c9d14520c64bc81
github username <jhernandez1692@gmail.com>
2020-10-13 12:23:35 -0400

    logout route and user views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,20 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+
+    # returns a boolean for if user is logged in or not
+    def logged_in?
+    end
+
+
+    # keeps track of logged in user
+    def current_user
+      User.find_by(id: session[:user_id])
+
+    end
+
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,58 @@
 class UsersController < ApplicationController
 
     get '/login' do
-        erb :login
+        erb :'/users/login'
     end
 
     post '/login' do
        user = User.find_by(email: params[:email])
-       binding.pry
+       
+    #    binding.pry
        if user && user.authenticate(params[:password])
         # creating a session for a user logs them in (creating a key value pair in the sessions hash)
+        session[:user_id] = user.id
         # take to home page
+        redirect "/users/#{user.id}"
        else
         # error
         # try again
+       end
     
-        redirect '/login'
+        redirect '/users/login'
+    end
+
+    get '/users/:id' do
+        
+        "user profile!"
+        @user = User.find_by(id: session[:id])
+
+        erb :'/users/show'
+    end
+
+    get '/signup' do
+        
+        erb :'/users/signup'
+    end
+
+
+    # post signup route that recieves input data from the user, creates the user, and 
+    # logs the user in
+    post '/users' do
+        # will need to add validations to make sure all inputs are filled out before
+        # creating a user
+        @user = User.create(params)
+        # add key value pair to session hash
+        session[:user_id] = @user.id
+        # redirect user to profile
+        redirect "/users/#{@user.id}"
+    end
+
+    # get route that clears the session hash, logging the user out
+    get '/logout' do
+        session.clear
+
+        #redirect to home page
+        redirect '/'
     end
 
 
