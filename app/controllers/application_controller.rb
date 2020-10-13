c78a9133035e8a52e22e2a8c2271d4ddb8a4b5a0
github username <jhernandez1692@gmail.com>
2020-10-13 11:00:04 -0400

    enabled sessions and authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # enable sessions
+    enable :sessions
+    # set session secret for extra security
+    set :session_secret, "secret_session"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    post '/login' do
+       user = User.find_by(email: params[:email])
+       binding.pry
+       if user && user.authenticate(params[:password])
+        # creating a session for a user logs them in (creating a key value pair in the sessions hash)
+        # take to home page
+       else
+        # error
+        # try again
+    
+        redirect '/login'
+    end
+
 
 end
