fd50c3e955310ae76bed86f2f609175305d0126b
github username <jhernandez1692@gmail.com>
2020-10-13 19:36:33 -0400

    Completed Patch and Delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,19 +20,17 @@ class PostsController < ApplicationController
         redirect "/posts/#{post.id}"
     end
 
-    # ORDER MATTERS = READ Functionality needs CREATE to go first
+    # READ Functionality needs CREATE to go first
     # READ
-    # index route for all posts
-    
 
+    # index route for all posts
     get '/posts' do
         # render all posts
-        
         @posts = Post.all
-        
-        
+        # render index page with all posts belonging to the user
         erb :'/posts/index'
     end
+
     # show route for single posts
     get '/posts/:id' do
         @post = Post.find(params[:id])
@@ -42,9 +40,34 @@ class PostsController < ApplicationController
 
 
     # UPDATE
+    # need to have Rack::MethodOverrride in config.ru to make Patch and Delete requests
+    get '/posts/:id/edit' do
+        # find correct post to render in form for edit.
+        @post = Post.find(params[:id])
 
+        erb :'/posts/edit'
+    end
+
+    patch '/posts/:id' do
+        # find correct post to edit
+        @post = Post.find(params[:id])
+        # use update to take edits from params and update the post in the database
+        @post.update(title: params[:title], description: params[:description],
+        image_url: params[:image_url]) 
+        # redirect to show page to view updated post
+        redirect "/posts/#{@post.id}"
+    end
 
     # DELETE
 
+    delete '/posts/:id' do
+        # find post from :id param
+        # dont need find_by because we are using :id to search
+        @post = Post.find(params[:id])
+        # destroy post/ delete from database
+        @post.destroy
+        # redirect back to index page to show all post, minus the one that was deleted
+        redirect '/posts'
+    end
 
 end
--- config.ru
+++ config.ru
@@ -4,6 +4,10 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+# in order to send patch and delete requests:
+use Rack::MethodOverride
+
+# mounting seperate controllers
 use UsersController
 use PostsController
 
